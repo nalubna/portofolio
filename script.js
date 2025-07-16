f10e39a9fbacf3d87e685f3ad6d70a1b0c43ee21
nalubna <hannanashita0@gmal.com>
2025-07-16 15:13:25 +0700

    add interactive navbar

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
 const form = document.getElementById("myForm");
 
 form.addEventListener("submit", (event) => {
-    event.preventDefault();
-    const name = document.getElementById("name").value;
-    const message = document.getElementById("message").value;
-    const formattedName = encodeURIComponent(name);
-    const formattedMessage = encodeURIComponent(message);
-    const whatsappUrl = `https://wa.me/6287772564987?text=${formattedName}%0A${formattedMessage}`
-    window.open(whatsappUrl);
+  event.preventDefault();
+  const name = document.getElementById("name").value;
+  const message = document.getElementById("message").value;
+  const formattedName = encodeURIComponent(name);
+  const formattedMessage = encodeURIComponent(message);
+  const whatsappUrl = `https://wa.me/6287772564987?text=${formattedName}%0A${formattedMessage}`;
+  window.open(whatsappUrl);
 });
+
+function showSidebar() {
+  const sidebar = document.querySelector(".sidebar");
+  sidebar.style.display = "flex";
+}
+
+function hideSidebar() {
+  const sidebar = document.querySelector(".sidebar");
+  sidebar.style.display = "none";
+}
